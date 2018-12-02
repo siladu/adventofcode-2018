8a6504851aeda7989c30e430ba4c418ce23a639b
simon.dudley <simon.dudley@autotrader.co.uk>
2018-12-02 12:01:52 +0000

    Added day two part one improved

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,6 +1,6 @@
 object Day2 {
 
-    private fun runPartOne(input: List<String>) {
+    private fun partOne(input: List<String>) {
 
         fun matchesFor(word: String): Set<Int> {
             return word
@@ -16,9 +16,20 @@ object Day2 {
         println(theTwos.size * theThrees.size)
     }
 
+    private fun partOneImproved(input: List<String>) {
+        // https://www.reddit.com/r/adventofcode/comments/a2aimr/2018_day_2_solutions/eawrg2y
+        val charCounts: List<Collection<Int>> = input.map { word ->
+            word.groupingBy { it }.eachCount().values
+        }
+
+        val twos = charCounts.count { 2 in it }
+        val threes = charCounts.count { 3 in it }
+        println(twos * threes)
+    }
+
     data class Comparision(val word: String, val other: String, val commonLetters: List<Char>, val diff: Int)
 
-    private fun runPartTwo(input: List<String>) {
+    private fun partTwo(input: List<String>) {
 
         fun letterComparison(word: String, other: String): Comparision {
             val common = word.zip(other).filter { it.first == it.second }.map { it.first }
@@ -35,8 +46,9 @@ object Day2 {
     @JvmStatic
     fun main(args: Array<String>) {
         Runner.timedRun("day2-input.txt") {
-            runPartOne(it)
-            runPartTwo(it)
+            partOne(it)
+            partOneImproved(it)
+            partTwo(it)
         }
     }
 }
