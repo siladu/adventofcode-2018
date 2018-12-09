5832e9bc882c02b6f865782b93a7d1fd75519201
simon.dudley <simon.dudley@autotrader.co.uk>
2018-12-09 12:28:58 +0000

    Day 9 Part 2 complete - using LinkedList along with a listIterator.

--- src/Day9.kt
+++ src/Day9.kt
@@ -7,13 +7,15 @@ object Day9 {
         val numPlayers = split[0].toInt()
         val lastMarbleValue = split[6].toInt()
 
-        var currentMarbleIdx = 0
+//        var currentMarbleIdx = 0
         var currentPlayer = 0
-        val marbles = mutableListOf<Int>()
+        val marbles: LinkedList<Int> = LinkedList()
         marbles.add(0)
-        var playerScores = (1..numPlayers).associate { (it to 0) }.toMutableMap()
+        var iterator = marbles.listIterator()
+
+        var playerScores = (1..numPlayers).associate { (it to 0L) }.toMutableMap()
         println("$numPlayers players; last marble is worth $lastMarbleValue points")
-        println("[-] ${printableMarbles(marbles, currentMarbleIdx).joinToString()}")
+        println("[-] ${printableMarbles(marbles, 0).joinToString()}")
 
         val addTimings = mutableListOf<Long>()
         var firstAddAverage = 0.0
@@ -23,24 +25,36 @@ object Day9 {
         for (marbleValue in (1..lastMarbleValue)) {
 
             if (marbleValue % 23 == 0) {
-                val toRemove = Math.floorMod((currentMarbleIdx - 7), marbles.size)
-                currentMarbleIdx = toRemove
-
+//                val toRemove = Math.floorMod((currentMarbleIdx - 7), marbles.size)
+//                currentMarbleIdx = toRemove
+//
                 val start = System.nanoTime()
-                val removedMarble = marbles.removeAt(toRemove)
+                for (i in (0..7)) {
+                    if (!iterator.hasPrevious()) iterator = marbles.listIterator(marbles.size)
+                    iterator.previous()
+                }
+                val removedMarble = iterator.next()
+                iterator.previous()
+                iterator.remove()
+                iterator.next()
+//                val removedMarble = marbles.removeAt(toRemove)
                 removeTimings += System.nanoTime() - start
-
+//
                 val newScore = marbleValue + removedMarble
                 val currentScore = playerScores[currentPlayer + 1] ?: 0
                 playerScores[currentPlayer + 1] = currentScore + newScore
             } else {
-                val nextIndex = getNextIndex(currentMarbleIdx, marbles)
+//                val nextIndex = getNextIndex(currentMarbleIdx, marbles)
 
                 val start = System.nanoTime()
-                marbles.add(nextIndex, marbleValue)
+                if (!iterator.hasNext()) iterator = marbles.listIterator(0)
+                iterator.next()
+                iterator.add(marbleValue)
+//                marbles.add(nextIndex, marbleValue)
                 addTimings += System.nanoTime() - start
 
-                currentMarbleIdx = nextIndex
+//                currentMarbleIdx = nextIndex
+
             }
 
             if (marbleValue % 10000 == 0) {
@@ -50,11 +64,11 @@ object Day9 {
                     firstAddAverage = addAvg
                     firstRemoveAverage = remAvg
                 }
-                println("add : $addAvg ${(addAvg / firstAddAverage).toInt()}X slower")
-                println("rem : $remAvg ${(remAvg / firstRemoveAverage).toInt()}X slower")
+                println("add : $addAvg ${(addAvg / firstAddAverage).toInt()}X slower - at marble value $marbleValue")
+                println("rem : $remAvg ${(remAvg / firstRemoveAverage).toInt()}X slower - at marble value $marbleValue")
             }
 
-//            println("[${currentPlayer + 1}] ${printableMarbles(marbles, currentMarbleIdx).joinToString()}")
+//            println("[${currentPlayer + 1}] ${printableMarbles(marbles, iterator.nextIndex() - 1).joinToString()}")
 
             currentPlayer = (currentPlayer + 1) % numPlayers
         }
@@ -83,8 +97,8 @@ object Day9 {
         val testInput4 = "21 players; last marble is worth 6111 points"//: high score is 54718"
         val testInput5 = "30 players; last marble is worth 5807 points"//: high score is 37305"
         val input = "478 players; last marble is worth 71240 points" // 375465
-        val input2 = "478 players; last marble is worth 7124000 points"
-        val input3 = "478 players; last marble is worth 712400 points"
+        val partTwoTestInput = "478 players; last marble is worth 712400 points" //31212086
+        val inputPartTwo = "478 players; last marble is worth 7124000 points" //3037741441
 
         Runner.timedRun(8) {
 //            run(testInput)
@@ -94,8 +108,8 @@ object Day9 {
 //            run(testInput4)
 //            run(testInput5)
 //            run(input)
-            run(input3)
-//            run(input2)
+            run(inputPartTwo)
+//            run(partTwoTestInput)
         }
     }
 }
