89004d5c862f37435b1c974e25e0c91eaec010fd
simon.dudley <simon.dudley@autotrader.co.uk>
2018-12-17 22:37:39 +0000

    Day 14 Part 2

--- src/Day14.kt
+++ src/Day14.kt
@@ -20,7 +20,8 @@ object Day14 {
      */
 
     data class Elf(var pos: Int, var score: Int)
-    private fun run(input: Int) {
+
+    private fun run(input: Int, partTwoInput: String) {
         val elf1 = Elf(0, 3)
         val elf2 = Elf(1, 7)
         val output: MutableList<Int> = mutableListOf(3, 7)
@@ -28,6 +29,13 @@ object Day14 {
 
         val max = input + 10
         for (i in 1..max) {
+
+            val fromIndex = kotlin.math.max(i - partTwoInput.length, 0)
+            val trimmedOutput: String = output.subList(fromIndex, i).joinToString("")
+            if (trimmedOutput == partTwoInput) {
+                println("PART TWO $fromIndex")
+            }
+
             val sum = elf1.score + elf2.score
             val sumAsString: List<Int> = sum.toString().map { it.toString().toInt() }
             output.addAll(sumAsString)
@@ -37,19 +45,26 @@ object Day14 {
             elf2.pos = move(elf2)
             elf2.score = output[elf2.pos]
 
-//            println(i)
         }
-        println("RES " + output.drop(input).take(10).joinToString(""))
+        println("PART ONE " + output.drop(input).take(10).joinToString(""))
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
 
+        /*
+        51589 first appears after 9 recipes.
+        01245 first appears after 5 recipes.
+        92510 first appears after 18 recipes.
+        59414 first appears after 2018 recipes.
+         */
         Runner.timedRun(14) { input ->
-//            run(5) //0124515891
-//            run(18) //9251071085
-//            run(2018) //5941429882
-            run(846601) //3811491411
+//            run(9, "51589") //0124515891
+//            run(5, "01245") //0124515891
+//            run(18, "92510") //9251071085
+//            run(2018, "59414") //5941429882
+//            run(846601, "846601") //3811491411
+            run(20408083, "846601") //20408083
         }
     }
 }
