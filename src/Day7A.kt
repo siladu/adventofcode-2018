63871718e60b8f8e075a8bb0ac8a51ed80ac99e9
simon.dudley <simon.dudley@autotrader.co.uk>
2019-01-08 08:25:11 +0000

    Remove unused code and excess printlns from Day7A

--- src/Day7A.kt
+++ src/Day7A.kt
@@ -4,37 +4,29 @@ object Day7A {
         // https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
         val pairs = input.map { it.split(" ") }.map { Pair(it[1], it[7]) }
         val graph: MutableMap<String, MutableList<String>> = pairs.groupBy({ it.first }, { it.second }).mapValues { it.value.toMutableList() }.toMutableMap()
-//        println(pairs)
-//        println(graph)
+
 //        L ← Empty list that will contain the sorted elements
         val sorted = mutableListOf<String>()
 //        S ← Set of all nodes with no incoming edge
         val startNodes: MutableSet<String> = graph.keys.filter { n -> graph.values.none { it.contains(n) } }.toSortedSet()
-//        println(startNodes)
+
 //        while S is non-empty do
         while (startNodes.isNotEmpty()) {
-//            println("***********************")
-//            println("START NODES $startNodes")
             val node = startNodes.first()
 //            remove a node n from S
             startNodes.remove(node)
 //            add n to tail of L
             sorted.add(node)
-//            println("sorted $sorted")
             val edges: List<String> = graph.getOrDefault(node, emptyList<String>()).toList() //copy the list
-//            println(edges)
 //            for each node m with an edge e from n to m do
             edges.forEach { edge ->
 //              remove edge e from the graph
-//                println("edge: $edge")
                 graph[node]?.remove(edge)
-//                println("graph: $graph")
 
 //              if m has no other incoming edges then
                 if (graph.values.none { it.contains(edge) }) {
 //                  insert m into S
                     startNodes.add(edge)
-//                    println("startNodes $startNodes")
                 }
             }
         }
@@ -62,37 +54,25 @@ object Day7A {
             println("$totalTime | $workerString")
         }
 
-//        while (traversalPath.length < numSteps){
         while(sorted.size < numUniqueChars) {
-            // iterate over workers[] to check for completed steps.
+
             val completedSteps = workers.filter { w -> w.work?.remaining == 0 }
-//            println("completedSteps $completedSteps")
-            // delete those steps from the DAG
+
             val preFilteredAvailableSteps: Set<String> = completedSteps
                 .flatMap { graph[it.work?.item].orEmpty() }
                 .toSortedSet()
+
             completedSteps.forEach { w -> graph.remove(w.work?.item) }
-            val availableSteps = preFilteredAvailableSteps
-                .filterNot { n -> graph.values.any { it.contains(n) } }
-//            println("graph $graph")
-            // set the those workers to idle
             completedSteps.forEach { w -> w.work = null }
 
-            // pull next availableSteps
+            val availableSteps = preFilteredAvailableSteps
+                .filterNot { n -> graph.values.any { it.contains(n) } }
             startNodes += availableSteps
-//            startNodes += graph.keys
-//                .filter { n -> graph.values.none { it.contains(n) } }
-//                .filterNot { n -> workers.any { it.work?.item == n } }
-//                .toSortedSet()
 
-            // assign steps to idle workers until you run out of steps or workers.
             while (workers.any { it.isFree() } && !startNodes.isEmpty()) {
                 val nextFreeItem = startNodes.first()
                 startNodes.remove(nextFreeItem)
                 sorted += nextFreeItem
-//                println("nextFreeItem $nextFreeItem")
-//                println("startNodes $startNodes")
-//                println("sorted $sorted")
                 workers.first { it.isFree() }.work = Work(nextFreeItem, calculateRemaining(nextFreeItem[0]))
             }
 
@@ -102,7 +82,7 @@ object Day7A {
             totalTime++
         }
         while (workers.any { !it.isFree() }) {
-            // tick busy workers
+            // tick remaining busy workers
             printWorkers()
             workers.filterNot { it.isFree() }.forEach { it.work!!.remaining-- }
             val completedSteps = workers.filter { w -> w.work?.remaining == 0 }
@@ -129,8 +109,8 @@ object Day7A {
         // BGKDMJCNEQRSTUZWHYLPAFIVXO
         Runner.timedRun(7) { input ->
             run(input)
-//            partTwo(testInput, 2, 0, 6)
-            partTwo(input, 5, 60, 26)
+//            partTwo(testInput, 2, 0, 6) //15
+            partTwo(input, 5, 60, 26) // 941
         }
     }
 }
